df57b75aa514a1a619e4184c853f2f78bcbfafd4
Maya Farberov <mayafl2020@gmail.com>
2023-10-31 10:02:36 -0400

    db project for jenknis

---
+++ src/test/java/co/wedevx/digitalbank/automation/ui/runners/RunCucumberClass.java
@@ -0,0 +1,17 @@
+package co.wedevx.digitalbank.automation.ui.runners;
+
+import org.junit.platform.suite.api.*;
+import org.testng.ISuite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("ui/features/checking")
+//@ConfigurationParameter(key = GLUE_PROPERTY_NAME , value = "steps")
+@ConfigurationParameter(key=PLUGIN_PROPERTY_NAME , value = "pretty,html:target/results.html")
+public class RunCucumberClass {
+
+}
